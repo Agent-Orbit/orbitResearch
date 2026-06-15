404a520f83fa3b86d4d9f95c8271f2cb23da9087
AliAkbar4025 <syedaliakbar40.25@gmail.com>
2026-06-15 16:24:41 +0500

    Update: URL opener

--- tools/webSearch.py
+++ tools/webSearch.py
@@ -1,22 +1,77 @@
-import requests
+from langchain_tavily import TavilySearch,TavilyExtract
+from langchain_core.tools import tool
 from utils.apiKeys import TAVILY_API_KEY
+import os
+from urllib.parse import urlparse
 
-def searchTool(query: str):
-    """ Searhces Tavily for given query """
+os.environ["TAVILY_API_KEY"] = TAVILY_API_KEY
 
-    url = "https://api.tavily.com/search"
+searchTool = TavilySearch()
+extractTool = TavilyExtract()
 
-    headers = {
-        "Authorization": f"{TAVILY_API_KEY}",
-        "Content-Type": "application/json"
-    }
+@tool
+def search(query: str, max_results: int = 5, search_depth = "basic"):
+    """ Searches web for query """
 
-    payload = {
-        "query": query,
-        "search_depth": "advanced",
-        "max_results": 5
-    }
+    try:
 
-    response = requests.post(url, json=payload, headers=headers)
+        return searchTool.invoke({
+            "query": query,
+            "max_results": max_results,
+            "search_depth": search_depth
+        })
 
-    print(response.json()["results"][0])
+    except Exception as e:
+
+        raise e
+
+@tool
+def extractWeb(url: str):
+    """ Opens given URL and extracts its contents """
+
+    try:
+
+        if not is_url(url):
+
+            return "Given URL is incorrect"
+
+        # Make URLs list later.
+        result = extractTool.invoke({"urls": [url]})
+
+        if result.get("results"):
+
+            content = result["results"][0].get("raw_content", "")
+            max_chars = 8000
+
+            if len(content) > max_chars:
+
+                content = content[:max_chars] + "\n\n...[truncated]"
+
+            return content
+
+        if result.get("failed_results"):
+
+            return f"Failed to extract: {result['failed_results']}"
+
+        return "No content extracted"
+    
+    except Exception as e:
+
+        raise e
+
+
+def is_url(url: str):
+    """ Checkes if given string is an URL or not """
+
+    try:
+
+        result = urlparse(url)
+
+        return all([
+            result.scheme,
+            result.netloc
+        ])
+    
+    except Exception as e:
+
+        return f"Arxiv search failed: {e}"
